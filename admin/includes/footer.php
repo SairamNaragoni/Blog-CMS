10ade9c317eb2d9bf310e02b6a0e82aa9a96b537
Sairam <naragonisairam@gmail.com>
2017-08-29 14:45:39 +0530

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+    <!-- Bootstrap core JavaScript -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
